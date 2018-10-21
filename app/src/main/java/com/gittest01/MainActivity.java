45658248ba2e0cd528a8efa2cd219a3acfa20b98
lxrongdke@163.com <lxrongdke@163.com>
2018-10-21 14:42:49 +0800

    第二次提交

--- app/src/main/java/com/gittest01/MainActivity.java
+++ app/src/main/java/com/gittest01/MainActivity.java
@@ -2,12 +2,16 @@ package com.gittest01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  String TAG=getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: 我是更改的内容");
+        Log.d(TAG, "onCreate: 第一次添加数据");
     }
 }
