f8c4a814d658b1eb814627bd7419b1cd7347375e
lxrongdke@163.com <lxrongdke@163.com>
2018-10-21 14:45:07 +0800

    第三次提交数据

--- app/src/main/java/com/gittest01/MainActivity.java
+++ app/src/main/java/com/gittest01/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: 我是更改的内容");
         Log.d(TAG, "onCreate: 第一次添加数据");
+        Log.d(TAG, "onCreate: 第二次添加数据");
     }
 }
